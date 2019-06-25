0d9d65d42047b874e9726ac56a18b80356423dbb
Matt Chalmers <matt@barefootnomads.com>
2019-06-26 01:29:13 +1000

    Cleanup unused code and dependencies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-import {getPricingRules} from "./Deals";
-import {CheckOut} from "./CheckOut";
 
 
-console.info('Not yet implemented');
-
-
-let customerId = 5;
-let pricingRules = getPricingRules(customerId);
-let checkOut = new CheckOut(pricingRules);
-
-for (let i = 0; i < 10000; i++) {
-    checkOut.add('classic');
-}
-
-console.log(checkOut.total());
